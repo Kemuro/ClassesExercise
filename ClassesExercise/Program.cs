025aa75566773a740b1cfaf3aa7731b2b647aa94
Kemuro <antonkemuro@gmail.com>
2021-04-12 13:40:14 -0500

    created classesExercise

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make ="Audi";
+            car1.Model ="R8 Spyder";
+            car1.Year = 2021;
+
+            Console.WriteLine($"Make: {car1.Make}");
+            Console.WriteLine($"Model: {car1.Model}");
+            Console.WriteLine($"Year: {car1.Year}");
+
+        }
+    }
+}
